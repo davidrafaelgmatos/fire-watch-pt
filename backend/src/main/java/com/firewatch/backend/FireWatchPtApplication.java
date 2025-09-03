a7290ac8cf79361b5c0269f2a38f0cdd19998d84
davidrafaelgmatos <david_matos_96@hotmail.com>
2025-09-03 13:04:55 +0100

    init spring backend

---
+++ backend/src/main/java/com/firewatch/backend/FireWatchPtApplication.java
@@ -0,0 +1,13 @@
+package com.firewatch.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireWatchPtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireWatchPtApplication.class, args);
+	}
+
+}
