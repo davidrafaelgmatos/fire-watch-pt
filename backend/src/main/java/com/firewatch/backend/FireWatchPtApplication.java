40d5f6a19afeca0ea0de1c833d3069f82a49658a
davidrafaelgmatos <david_matos_96@hotmail.com>
2025-09-05 20:47:53 +0100

    Changes to app

--- backend/src/main/java/com/firewatch/backend/FireWatchPtApplication.java
+++ backend/src/main/java/com/firewatch/backend/FireWatchPtApplication.java
@@ -2,8 +2,16 @@ package com.firewatch.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.http.client.HttpClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.client.RestClientAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+//@SpringBootApplication
+@EnableScheduling
+@SpringBootApplication(exclude = {
+		HttpClientAutoConfiguration.class,
+		RestClientAutoConfiguration.class
+})
 public class FireWatchPtApplication {
 
 	public static void main(String[] args) {
